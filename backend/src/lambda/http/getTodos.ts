84cd6854ec14de804d2c1ff3b57b2c5d239fabd6
Vlad Flore <flore.vlad@gmail.com>
2021-04-03 22:02:24 +0200

    Implement get todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,11 +1,25 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+
+  const result = await dynamodb.scan({
+    TableName: process.env.TODOS_TABLE
+  }).promise()
+
+  const todos = result.Items
+
   return {
     statusCode: 200,
-    body: JSON.stringify({})
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todos
+    })
   }
 }
