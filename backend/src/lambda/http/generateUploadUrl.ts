bc035aafe0c9e1ffccd6679160f21e2ace059e06
Vlad Flore <flore.vlad@gmail.com>
2021-04-04 00:54:59 +0200

    Implement generate upload url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,67 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const result = await dynamodb.get({
+    TableName: process.env.TODOS_TABLE,
+    Key: {
+      //TODO fetch the user id dynamically
+      userId: '2eaab214-16ce-4112-8417-342cec37ce85',
+      todoId: todoId
+    }
+  }).promise()
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  if (!result.Item) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: `Todo with id ${todoId} does not exit!`
+      })
+    }
+  }
+
+  await dynamodb.update({
+    TableName: process.env.TODOS_TABLE,
+    Key: {
+      //TODO fetch the user id dynamically
+      userId: '2eaab214-16ce-4112-8417-342cec37ce85',
+      todoId: todoId
+    },
+    UpdateExpression: 'set attachmentUrl = :url',
+    ExpressionAttributeValues: {
+      ':url': `https://${process.env.TODO_IMAGES_S3_BUCKET}.s3.amazonaws.com/${todoId}`
+    }
+  }).promise()
+
+  const uploadUrl = getUploadUrl(todoId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
+
+}
+
+function getUploadUrl(todoId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: process.env.TODO_IMAGES_S3_BUCKET,
+    Key: todoId,
+    Expires: 300
+  })
 }
