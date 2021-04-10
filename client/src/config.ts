8e510222a4e3d913e1720590223b62b363b99b14
Vlad Flore <flore.vlad@gmail.com>
2021-04-11 00:45:01 +0200

    Change config to target current aws deployment and Auth0 app

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'wyckueodz4'
+const region = 'eu-central-1'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'udacity-serverless-udagram.eu.auth0.com',
+  clientId: 'ov36eWfUNmkGdiUEcUqv7ZrZR82Dg4gC',
+  callbackUrl: 'http://localhost:3000/callback'
+}
